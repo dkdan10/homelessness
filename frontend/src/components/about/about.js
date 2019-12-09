fcf3f08ad1fbaee8c045ae496c334f20a5c5ad5c
dkdan10 <danielkeinan0@gmail.com>
2019-12-09 01:42:17 -0500

    get light picker to update graph range

--- frontend/src/components/about/about.js
+++ frontend/src/components/about/about.js
@@ -1,5 +1,8 @@
-import React, {useEffect, useRef} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import * as d3 from 'd3'
+
+import Lightpick from 'lightpick'
+import "./lightpick.css"
 
 function About(props) {
     const {
@@ -7,7 +10,67 @@ function About(props) {
         homelessShelterCensusData
     } = props
 
-    const scatterRef = useRef(null) 
+    const [minDate, setMinDate] = useState(0)
+    const [maxDate, setMaxDate] = useState(0)
+    const [lightPicker, setLightPicker] = useState(null)
+
+    const scatterRef = useRef(null)
+    const datePickerStartRef = useRef(null)
+    const datePickerEndRef = useRef(null)
+
+    // if (minDate && maxDate && datePickerStartRef.current && datePickerEndRef.current && !lightPicker) {
+    //     const pickerStart = datePickerStartRef.current
+    //     const pickerEnd = datePickerEndRef.current
+    //     let lastMin = null
+    //     let lastMax = null
+    //     const datePicker = new Lightpick({
+    //         field: pickerStart,
+    //         secondField: pickerEnd,
+    //         singleDate: false,
+    //         selectForward: true,
+    //         repick: true,
+    //         onSelect: (start, end) => {
+    //             console.log("called")
+    //             if (!lastMin || start.unix() !== lastMin.unix()) {
+    //                 lastMin = start
+    //                 setMinDate(start)
+    //             }
+    //             if (!lastMax || end.unix() !== lastMax.unix()) {
+    //                 lastMax = end
+    //                 setMaxDate(end)
+    //             }
+    //         }
+    //     })
+    //     datePicker.setDateRange(new Date(minDate), new Date(maxDate))
+        // setLightPicker(datePicker)
+    // }
+
+    const shouldSetDatePicker = !!datePickerStartRef && !!datePickerEndRef
+    useEffect(() => {
+        const pickerStart = datePickerStartRef.current
+        const pickerEnd = datePickerEndRef.current
+        let lastMin = null
+        let lastMax = null
+        const datePicker = new Lightpick({
+            field: pickerStart,
+            secondField: pickerEnd,
+            singleDate: false,
+            selectForward: true,
+            repick: true,
+            onSelect: (start, end) => {
+                console.log(start, end)
+                if (!lastMin || start.unix() !== lastMin.unix()) {
+                    lastMin = start
+                    setMinDate(start)
+                }
+                if (!lastMax || end.unix() !== lastMax.unix()) {
+                    lastMax = end
+                    setMaxDate(end)
+                }
+            }
+        })
+        setLightPicker(datePicker)
+    }, [shouldSetDatePicker])
 
     useEffect(() => {
         getHomelessShelterCensusData()
@@ -15,90 +78,103 @@ function About(props) {
 
     useEffect(() => {
         createScatterPlot()
-    }, [homelessShelterCensusData])
 
-    function createScatterPlot () {
-        console.log(homelessShelterCensusData)
-        const margin = { top: 10, right: 30, bottom: 60, left: 60 },
-            width = 460 - margin.left - margin.right,
-            height = 400 - margin.top - margin.bottom;
+        function createScatterPlot() {
+            const margin = { top: 10, right: 30, bottom: 60, left: 60 },
+                width = 460 - margin.left - margin.right,
+                height = 400 - margin.top - margin.bottom;
 
-        d3.select(scatterRef.current)
-            .selectAll("*")
-            .remove()
+            d3.select(scatterRef.current)
+                .selectAll("*")
+                .remove()
 
-        const svg = d3.select(scatterRef.current)
-            .append("svg")
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform",
-                "translate(" + margin.left + "," + margin.top + ")");
+            const svg = d3.select(scatterRef.current)
+                .append("svg")
+                .attr("width", width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+                .append("g")
+                .attr("transform",
+                    "translate(" + margin.left + "," + margin.top + ")");
 
-        const data = homelessShelterCensusData
-
-        const parseTime = d3.timeParse("%d-%b-%y");
-        // var parseTime = d3.timeParse("%d-%b-%y");
-        // data.forEach(function (d) {
-        //     d.date_of_census = parseTime(d.date_of_census);
-        // });
-
-        const x = d3.scaleLinear()
-            .domain(d3.extent(data, function (d) { return Date.parse(d.date_of_census); }))
-            .range([0, width]);
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x)
-                .tickFormat(d3.timeFormat("%Y-%m-%d")))
-            .selectAll("text")
-            .style("text-anchor", "end")
-            .attr("dx", "-.8em")
-            .attr("dy", ".15em")
-            .attr("transform", "rotate(-65)");
-
-        const y = d3.scaleLinear()
-            .domain([57000, 62000])
-            .range([height, 0]);
-        svg.append("g")
-            .call(d3.axisLeft(y));
-
-        svg.append('g')
-            .selectAll("dot")
-            .data(data)
-            .enter()
-            .append("circle")
-            .attr("cx", function (d) { return x(Date.parse(d.date_of_census)); })
-            .attr("cy", function (d) { return y(parseInt(d.total_individuals_in_shelter)); })
-            .attr("r", 1.5)
-            .style("fill", "#69b3a2")
-            
-
-    }
-
-    function structureHomelessShelterCensusData() {
-        return (
-            homelessShelterCensusData.map((dataPoint, idx) => {
-                return (
-                    <li key={idx}>
-                        {dataPoint.date_of_census}
-                        <br/>
-                        {dataPoint.total_individuals_in_shelter}
-                        <br/>
-                        {dataPoint.total_single_adults_in_shelter}
-                        <br/>
-                        {dataPoint.families_with_children_in_shelter}
-                        <br/>
-                        {dataPoint.adult_families_in_shelter}
-                    </li>
-                )
+            const data = homelessShelterCensusData.map(datum => {
+                datum = Object.assign({}, datum)
+                datum.date_of_census = Date.parse(datum.date_of_census)
+                return datum
             })
-        )
-    }
+                .filter(datum => {
+                    if (minDate && maxDate) {
+                        return datum.date_of_census / 1000 > minDate.unix()
+                            && datum.date_of_census / 1000 < maxDate.unix()
+                    } else {
+                        return true
+                    }
+                })
+
+            const x = d3.scaleLinear()
+                .domain(d3.extent(data, function (d) { return d.date_of_census; }))
+                .range([0, width]);
+            svg.append("g")
+                .attr("transform", "translate(0," + height + ")")
+                .call(d3.axisBottom(x)
+                    .tickFormat(d3.timeFormat("%Y-%m-%d")))
+                .selectAll("text")
+                .style("text-anchor", "end")
+                .attr("dx", "-.8em")
+                .attr("dy", ".15em")
+                .attr("transform", "rotate(-65)");
+
+            const y = d3.scaleLinear()
+                .domain([57000, 62000])
+                .range([height, 0]);
+            svg.append("g")
+                .call(d3.axisLeft(y));
+
+            svg.append('g')
+                .selectAll("dot")
+                .data(data)
+                .enter()
+                .append("circle")
+                .attr("cx", function (d) { return x(d.date_of_census); })
+                .attr("cy", function (d) { return y(parseInt(d.total_individuals_in_shelter)); })
+                .attr("r", 1.5)
+                .style("fill", "#69b3a2")
+        }
+    }, [minDate, maxDate, homelessShelterCensusData])
+
+    useEffect(() => {
+        if (homelessShelterCensusData.length) {
+            const minDate = Date.parse(homelessShelterCensusData[homelessShelterCensusData.length - 1].date_of_census)
+            const maxDate = Date.parse(homelessShelterCensusData[0].date_of_census)
+            lightPicker.setDateRange(new Date(minDate), new Date(maxDate))
+        }
+    }, [homelessShelterCensusData, lightPicker])
+
+    // function structureHomelessShelterCensusData() {
+    //     return (
+    //         homelessShelterCensusData.map((dataPoint, idx) => {
+    //             return (
+    //                 <li key={idx}>
+    //                     {dataPoint.date_of_census}
+    //                     <br/>
+    //                     {dataPoint.total_individuals_in_shelter}
+    //                     <br/>
+    //                     {dataPoint.total_single_adults_in_shelter}
+    //                     <br/>
+    //                     {dataPoint.families_with_children_in_shelter}
+    //                     <br/>
+    //                     {dataPoint.adult_families_in_shelter}
+    //                 </li>
+    //             )
+    //         })
+    //     )
+    // }
 
     return (
         <div>
             {/* {structureHomelessShelterCensusData()} */}
             <div ref={scatterRef}></div>
+            <input type="text" ref={datePickerStartRef}></input>
+            <input type="text" ref={datePickerEndRef}></input>
         </div>
     )
 }
